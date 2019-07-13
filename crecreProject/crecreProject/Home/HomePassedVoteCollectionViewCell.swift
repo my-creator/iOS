abf5787e627d56325ee9b7b908be63c09ec6737b
chaeyeonhwang <victory6309@gmail.com>
2019-07-13 12:47:05 +0900

    APPJAM2

---
+++ crecreProject/crecreProject/Home/HomePassedVoteCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomePassedVoteCollectionViewCell.swift
+//  crecreProject
+//
+//  Created by 황채연 on 13/07/2019.
+//  Copyright © 2019 하준혁. All rights reserved.
+//
+
+import UIKit
+
+class HomePassedVoteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var HomePassedVoteImage: UIImageView!
+    
+    @IBOutlet weak var HomePassedVoteClassImage: UIImageView!
+    
+    @IBOutlet weak var HomePassedVoteRank: UILabel!
+    
+    @IBOutlet weak var HomePassedVoteTitle: UILabel!
+    
+    
+    
+}
